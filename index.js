18acc34a06fb9e81f734b5e6d2894bea340d706a
fahma20 <fahmaxo@gmail.com>
2024-09-30 14:33:59 +0300

    find method assignment

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// code your solution here
+function superbowlWin (record) {
+    const records = record.find (record => record.result === 'W')
+    return records ? records.year : undefined
+    
+}
